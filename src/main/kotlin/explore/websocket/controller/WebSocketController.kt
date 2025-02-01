61ac7fd437c810d20599a59d1a8933417cb09b69
jrgleason <jackiegleason@gmail.com>
2025-02-01 12:23:52 -0500

    More Changes

--- src/main/kotlin/explore/websocket/controller/WebSocketController.kt
+++ src/main/kotlin/explore/websocket/controller/WebSocketController.kt
@@ -2,16 +2,19 @@ package explore.websocket.controller
 
 import explore.websocket.model.Greeting
 import explore.websocket.model.TestMessage
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
 
 @Controller
 class WebSocketController {
+    private val logger = LoggerFactory.getLogger(WebSocketController::class.java)
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     fun greeting(message: TestMessage): Greeting {
-        println(message)
+        logger.info(message.toString())
         return Greeting("Hello, " + message.name + "!")
     }
 }
